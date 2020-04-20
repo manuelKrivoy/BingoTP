a45b0517669eaddcc8c3ff2d2dcba791b0c21efe
manu <manu@manu>
2020-04-20 13:34:27 -0300

    comento funcion de bingo.py

--- src/bingo.py
+++ src/bingo.py
@@ -1,3 +1,4 @@
+#defino un carton de ejemplo
 def carton():
     carton = (
         (0,1,0,1,1,0,1,0,0),
@@ -6,7 +7,7 @@ def carton():
     )
     return carton
 
-
+#defino una funcion que dado un determinado carton me devuelva lo que contienen sus 3 columnas
 def columna(carton, nro_columna):
     return(
         carton[0][nro_columna],
