905605f7361a299c0eca876ef1aa7de3b1b200c5
manu <manu@manu>
2020-04-20 13:55:13 -0300

    Comento las funciones de test

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,6 +1,6 @@
 from src.bingo import carton, columna
 
-
+#Funcion que verifica que no haya mas ni menos de 15 celdas ocupadas en un carton.
 def test_contar_celdas_ocupadas():
     mi_carton = carton()
     contador = 0
@@ -10,6 +10,7 @@ def test_contar_celdas_ocupadas():
 
     assert contador==15
 
+#Funcion que verifica que haya al menos una celda ocupada por columna.
 def test_al_menos_una_celda_por_columna():
     mi_carton = carton()
     contador_columnas = 0
@@ -21,6 +22,7 @@ def test_al_menos_una_celda_por_columna():
 
     assert check == 9
 
+#Funcion que verifica que cada fila tenga al menos una celda ocupada.
 def test_filas_no_vacias():
     mi_carton = carton()
     contador = 0
